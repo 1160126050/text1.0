13f6fbbae001329b69023c1a059b0a7dbc84a177
stu <stu@qq.com>
2016-10-31 18:04:22 +0800

    新增“数据库”、“博客和添加博文”按钮功能，其中按钮功能还未完善

---
+++ WebApplication1/BlogDatabase.cs
@@ -0,0 +1,41 @@
+﻿namespace WebApplication1
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class BlogDatabase : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“BlogDatabase”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“WebApplication1.BlogDatabase”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“BlogDatabase”
+        //连接字符串。
+        public BlogDatabase()
+            : base("name=BlogDatabase")
+        {
+        }
+        
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+         public virtual DbSet<Blog> Blogs { get; set; }
+         public virtual DbSet<BlogArticle> BlogArticles { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var blogTable = modelBuilder.Entity<Blog>();
+            var blogArtticleTable = modelBuilder.Entity<BlogArticle>();
+            blogTable.HasKey(o => o.Id);
+            blogArtticTable.HasKey(o => o.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var db = new BlogDatabase();
+            db.Database.CreateIfNotExists();
+            var lst = db.BlogArticles.OrderByDescending(o =>o.Id).ToList();
+            ViewBag.BlogArticles = lst;
+
+            return View();
+        }
+        public ActionResult AddArticle()
+        {
+            return View();
+
+        }
+        public ActionResult ArticleSave(string subject,string body)
+        {
+            var article = new BlogArticle();
+            article.Subject = subject;
+            article.Body = body;
+            article.DateCreated = DataTime.Now;
+            var db = new BlogDatabase();
+            db.BlogArticles.Add(article);
+            return Redirect("Index");
+        }
+    }
+}
